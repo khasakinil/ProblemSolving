d5f51c38555b8379e77fce2346cda4b5ed83f207
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-04-19 12:35:04 +0530

    Add files via upload
    example:
    Input1- {0 ,1 ,2 ,2 ,1 ,0}
    Input2- {0 ,1 ,2 ,1 ,2 ,1 ,2}

--- SortGivenList_012/src/com/sort/SortGivenList.java
+++ SortGivenList_012/src/com/sort/SortGivenList.java
@@ -9,22 +9,28 @@ public class SortGivenList {
 		int zerosPointer = 0;
 
 		for (int i = 0; i < numList.length; i++) {
-			if (numList[i] == 0 ) {
+			if (numList[i] == 0) {
+
+				zerosPointer = getNextZerosPointer(numList, zerosPointer);
+
 				if (i != zerosPointer && i > zerosPointer) {
 					int temp = numList[zerosPointer];
 					numList[zerosPointer] = numList[i];
 					numList[i] = temp;
+					zerosPointer++;
 				}
-				zerosPointer++;
 			}
 
-			if (numList[i] == 2 && i < twosPointer) {
-				if (i != twosPointer) {
+			if (numList[i] == 2) {
+
+				twosPointer = getNextTwoPointer(numList, twosPointer);
+
+				if (i != twosPointer && i < twosPointer) {
 					int temp = numList[twosPointer];
 					numList[twosPointer] = numList[i];
 					numList[i] = temp;
+					twosPointer--;
 				}
-				twosPointer--;
 
 				if (numList[i] == 0) {
 					i--;
@@ -33,8 +39,48 @@ public class SortGivenList {
 		}
 
 		for (Integer num : numList) {
-			System.out.println(num);
+			System.out.print(num + " ");
 		}
+	}
+
+	private static int getNextZerosPointer(int[] numList, int zerosPointer) {
+		int nextZerosPointer = zerosPointer;
+
+		boolean status = false;
+
+		if (numList[zerosPointer] == 0) {
+			status = true;
+		}
+
+		for (int i = zerosPointer; i < numList.length; i++) {
+			if (numList[i] == 0 && status) {
+				nextZerosPointer++;
+			} else {
+				status = false;
+			}
+		}
+
+		return nextZerosPointer;
+	}
+
+	private static int getNextTwoPointer(int[] numList, int twosPointer) {
+		int nextTwosPointer = twosPointer;
+
+		boolean status = false;
+
+		if (numList[twosPointer] == 2) {
+			status = true;
+		}
+
+		for (int i = twosPointer; i >= 0; i--) {
+			if (numList[i] == 2 && status) {
+				nextTwosPointer--;
+			} else {
+				status = false;
+			}
+		}
+
+		return nextTwosPointer;
 	}
 
 }
