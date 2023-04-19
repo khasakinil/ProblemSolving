78b943537f55ab594603c44fd39927e71ebd2e1d
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-04-19 11:46:52 +0530

    Add files via upload

--- SortGivenList_012/src/com/sort/SortGivenList.java
+++ SortGivenList_012/src/com/sort/SortGivenList.java
@@ -3,14 +3,14 @@ package com.sort;
 public class SortGivenList {
 
 	public static void main(String[] args) {
-		int[] numList = { 1, 1, 0, 2, 1, 0, 1, 2, 2, 1, 1, 0 };
+		int[] numList = { 0, 1, 2, 2, 1, 0 };
 
 		int twosPointer = numList.length - 1;
 		int zerosPointer = 0;
 
 		for (int i = 0; i < numList.length; i++) {
-			if (numList[i] == 0 && i > zerosPointer) {
-				if (i != zerosPointer) {
+			if (numList[i] == 0 ) {
+				if (i != zerosPointer && i > zerosPointer) {
 					int temp = numList[zerosPointer];
 					numList[zerosPointer] = numList[i];
 					numList[i] = temp;
