027570a745e94ff40180fb62a97d714c3c1106b1
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-19 23:18:48 +0530

    Add files via upload

---
+++ LongestCommonPrefixInArray/src/com/ps/LongestCommonPrefixInArray.java
@@ -0,0 +1,17 @@
+package com.ps;
+
+public class LongestCommonPrefixInArray {
+
+	public static void main(String[] args) {
+		String arr[] = { "geeksforgeeks", "geeks", "geek", "geezer" };
+		
+		String commonPrefix = getLongestCommonPrefix(arr);
+		
+		System.out.println("LongestCommonPrefixInArray : " + commonPrefix);
+	}
+
+	private static String getLongestCommonPrefix(String[] arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
