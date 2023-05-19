05d0d5eb8f6573a1c61f5f60d19051a08905d6a1
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-19 23:35:00 +0530

    Add files via upload

--- LongestCommonPrefixInArray/src/com/ps/LongestCommonPrefixInArray.java
+++ LongestCommonPrefixInArray/src/com/ps/LongestCommonPrefixInArray.java
@@ -12,10 +12,32 @@ public class LongestCommonPrefixInArray {
 
 	private static String getLongestCommonPrefix(String[] arr) {
 		String commonPrefix = "";
-		String baseStr = arr[0];
+
+		String baseStr = "";
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i].length() < arr[i + 1].length()) {
+				baseStr = arr[i];
+			}
+		}
+
 		for (int i = 0; i < baseStr.length(); i++) {
 			for (int j = i + 1; j < baseStr.length(); j++) {
-
+				String subStr = baseStr.substring(i, j);
+				boolean status = true;
+				for (int k = 0; k < arr.length; k++) {
+					if (arr[k].length() >= subStr.length()) {
+						if (!arr[k].contains(subStr)) {
+							status = false;
+							break;
+						}
+					}
+				}
+				if (status) {
+					if (commonPrefix.length() < subStr.length()) {
+						commonPrefix = subStr;
+					}
+				}
 			}
 		}
 
