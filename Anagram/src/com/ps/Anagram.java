7f1ae1ee02a0172d8ba83a751bff09513bcec900
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-20 13:24:12 +0530

    Anagram
    Given two strings a and b consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other.
    
    Input:a = geeksforgeeks, b = forgeeksgeeks
    Output: YES
    Explanation: Both the string have same characters with
            same frequency. So, both are anagrams.

---
+++ Anagram/src/com/ps/Anagram.java
@@ -0,0 +1,41 @@
+package com.ps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		String str1 = "geeksforgeeksw";
+		String str2 = "forgeeksgeeks";
+
+		String isAnagram = checkIsAnagram(str1, str2);
+		System.out.println("isAnagram : " + isAnagram);
+	}
+
+	private static String checkIsAnagram(String str1, String str2) {
+		Map<Character, Integer> str1CharCount = new HashMap<>();
+		Map<Character, Integer> str2CharCount = new HashMap<>();
+
+		String isAnagram = "YES";
+
+		for (int i = 0; i < str1.length(); i++) {
+			str1CharCount.put(str1.charAt(i), str1CharCount.getOrDefault(str1.charAt(i), 0) + 1);
+		}
+
+		for (int i = 0; i < str2.length(); i++) {
+			str2CharCount.put(str2.charAt(i), str2CharCount.getOrDefault(str2.charAt(i), 0) + 1);
+		}
+
+		for (Map.Entry<Character, Integer> map : str1CharCount.entrySet()) {
+			Character key = map.getKey();
+
+			if (str2CharCount.get(key) != map.getValue()) {
+				isAnagram = "NO";
+				break;
+			}
+		}
+
+		return isAnagram;
+	}
+}
