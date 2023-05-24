53e0f89206892c7afb1ce93a0586233976c32dfb
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-24 09:52:40 +0530

    Add files via upload

--- ValidateIPAddress/src/com/ps/ValidateIPAddress.java
+++ ValidateIPAddress/src/com/ps/ValidateIPAddress.java
@@ -3,7 +3,7 @@ package com.ps;
 public class ValidateIPAddress {
 
 	public static void main(String[] args) {
-		String ipAddress = "222.111.111.1112";
+		String ipAddress = "11..234";
 		boolean isValidIp = isValidIpAddress(ipAddress);
 		System.out.println("isValidIp : " + isValidIp);
 	}
@@ -12,13 +12,17 @@ public class ValidateIPAddress {
 		boolean isValid = true;
 
 		String[] ips = ipAddress.split("\\.");
-		System.out.println(ips);
-		for (int i = 0; i < ips.length; i++) {
-			if (!(Integer.valueOf(ips[i]) >= 0 && Integer.valueOf(ips[i]) < 266)) {
-				isValid=false;
-				break;
+		if (ips.length == 4) {
+			for (int i = 0; i < ips.length; i++) {
+				if (!(Integer.valueOf(ips[i]) >= 0 && Integer.valueOf(ips[i]) < 266)) {
+					isValid = false;
+					break;
+				}
 			}
+		} else {
+			isValid = false;
 		}
+
 		return isValid;
 	}
 }
