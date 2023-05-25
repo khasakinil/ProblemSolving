b9a5175fb117a537a9cb3a79731a0d881c616b8e
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-25 13:53:23 +0530

    Given a string S, find the length of the longest substring with all distinct characters.
    Input:
    S = "geeksforgeeks"
    Output: 7
    Explanation: "eksforg" is the longest 
    substring with all distinct characters.

---
+++ LongestDistinctCharactersInString/src/com/practice/LongestDistinctCharactersInString.java
@@ -0,0 +1,41 @@
+package com.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestDistinctCharactersInString {
+	public static void main(String[] args) {
+		String str = "geeksforgeeks";
+		String output = getLongestDistinctCharactersInString(str);
+		System.out.println("LongestDistinctCharactersInString: " + output);
+	}
+
+	private static String getLongestDistinctCharactersInString(String str) {
+		String longestString = "";
+
+		for (int i = 0; i <= str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				String temp = str.substring(i, j);
+				if (!isContainDuplicate(temp) && longestString.length() < temp.length()) {
+					longestString = temp;
+				}
+			}
+		}
+		return longestString;
+	}
+
+	private static boolean isContainDuplicate(String temp) {
+		boolean containDuplicate = false;
+
+		Map<Character, Integer> charCount = new HashMap<>();
+
+		for (int i = 0; i < temp.length(); i++) {
+			charCount.put(temp.charAt(i), charCount.getOrDefault(temp.charAt(i), 0) + 1);
+			if (charCount.get(temp.charAt(i)) > 1) {
+				containDuplicate = true;
+				break;
+			}
+		}
+		return containDuplicate;
+	}
+}
