14c8821a3a60d6eb4f7b6c7c7bf84c3a4219b707
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-29 23:20:04 +0530

    Add files via upload

--- IsomorphicStrings/src/com/practice/IsomorphicStrings.java
+++ IsomorphicStrings/src/com/practice/IsomorphicStrings.java
@@ -6,23 +6,29 @@ import java.util.Map;
 public class IsomorphicStrings {
 	public static void main(String args[]) {
 		String str1 = "aab";
-		String str2 = "xyz";
+		String str2 = "xty";
 		int isIsomorphic = IsomorphicString(str1, str2);
 		System.out.println("IsomorphicStrings : " + isIsomorphic);
 	}
 
 	private static int IsomorphicString(String str1, String str2) {
 
-		Map<Character, Character> isoMap = new HashMap<>();
+		Map<Character, Character> isoMapStr1 = new HashMap<>();
+		Map<Character, Character> isoMapStr2 = new HashMap<>();
 		if (str1.length() != str2.length()) {
 			return 0;
 		} else {
 			for (int i = 0; i < str1.length(); i++) {
-				if (isoMap.get(str1.charAt(i)) == null) {
-					isoMap.put(str1.charAt(i), str2.charAt(i));
+				if (isoMapStr1.get(str1.charAt(i)) == null) {
+					isoMapStr1.put(str1.charAt(i), str2.charAt(i));
 				}
 
-				if (str2.charAt(i) != isoMap.get(str1.charAt(i))) {
+				if (isoMapStr2.get(str2.charAt(i)) == null) {
+					isoMapStr2.put(str2.charAt(i), str1.charAt(i));
+				}
+
+				if (str2.charAt(i) != isoMapStr1.get(str1.charAt(i))
+						|| str1.charAt(i) != isoMapStr2.get(str2.charAt(i))) {
 					return 0;
 				}
 			}
