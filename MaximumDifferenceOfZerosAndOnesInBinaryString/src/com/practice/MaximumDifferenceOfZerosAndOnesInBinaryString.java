0e9b7027d71c0ac2c98d9fe9b8241a70f0f9b5e5
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-05-31 09:05:01 +0530

    Add files via upload

--- MaximumDifferenceOfZerosAndOnesInBinaryString/src/com/practice/MaximumDifferenceOfZerosAndOnesInBinaryString.java
+++ MaximumDifferenceOfZerosAndOnesInBinaryString/src/com/practice/MaximumDifferenceOfZerosAndOnesInBinaryString.java
@@ -11,10 +11,23 @@ public class MaximumDifferenceOfZerosAndOnesInBinaryString {
 	private static int getMaximumDifferenceOfZerosAndOnesInBinaryString(String str) {
 		int maxDiff = 0;
 
+		for (int i = 0; i < str.length(); i++) {
+			int count = 0;
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) != str.charAt(j)) {
+					count++;
+					continue;
+				}
+				if (maxDiff < count) {
+					maxDiff = count;
+				}
+				break;
+			}
+		}
 		if (maxDiff == 0) {
 			return -1;
 		}
-		return 0;
+		return maxDiff;
 	}
 
 }
