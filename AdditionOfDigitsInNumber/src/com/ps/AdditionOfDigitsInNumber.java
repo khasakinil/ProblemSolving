ac1f56c95f93ac13cf2318aa433b71144e0e58db
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-02 23:51:51 +0530

    Add files via upload

--- AdditionOfDigitsInNumber/src/com/ps/AdditionOfDigitsInNumber.java
+++ AdditionOfDigitsInNumber/src/com/ps/AdditionOfDigitsInNumber.java
@@ -2,13 +2,19 @@ package com.ps;
 
 public class AdditionOfDigitsInNumber {
 	public static void main(String args[]) {
-		int number = 12345;
+		int number = 1234;
 		int addtionOfDigits = performAdditionOfDigits(number);
 		System.out.println("AddtionOfDigits : " + addtionOfDigits);
 	}
 
 	private static int performAdditionOfDigits(int number) {
 		int digitAddition = 0;
+		int temp = 0;
+		while (number > 0) {
+			temp = number % 10;
+			digitAddition += temp;
+			number /= 10;
+		}
 		return digitAddition;
 	}
 }
