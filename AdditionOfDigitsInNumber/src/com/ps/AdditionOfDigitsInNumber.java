b7c58d24f4eb73e9c10d32dad15ed236cfb2a9db
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-03 23:27:14 +0530

    Add files via upload

--- AdditionOfDigitsInNumber/src/com/ps/AdditionOfDigitsInNumber.java
+++ AdditionOfDigitsInNumber/src/com/ps/AdditionOfDigitsInNumber.java
@@ -4,7 +4,23 @@ public class AdditionOfDigitsInNumber {
 	public static void main(String args[]) {
 		int number = 1234;
 		int addtionOfDigits = performAdditionOfDigits(number);
-		System.out.println("AddtionOfDigits : " + addtionOfDigits);
+		System.out.println("Addtion Of All Digits : " + addtionOfDigits);
+
+		int additionOfEvenDigits = getAdditionOfEvenDigits(number);
+		System.out.println("Addition Of Even Digits : " + additionOfEvenDigits);
+	}
+
+	private static int getAdditionOfEvenDigits(int number) {
+		int digitAddition = 0;
+		int temp = 0;
+		while (number > 0) {
+			temp = number % 10;
+			if (temp % 2 == 0) {
+				digitAddition += temp;
+			}
+			number /= 10;
+		}
+		return digitAddition;
 	}
 
 	private static int performAdditionOfDigits(int number) {
