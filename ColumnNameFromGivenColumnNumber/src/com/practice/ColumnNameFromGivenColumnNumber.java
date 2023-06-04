cd0443978501807465e4ccddf77506f1c152dc0f
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-04 19:35:28 +0530

    Add files via upload

--- ColumnNameFromGivenColumnNumber/src/com/practice/ColumnNameFromGivenColumnNumber.java
+++ ColumnNameFromGivenColumnNumber/src/com/practice/ColumnNameFromGivenColumnNumber.java
@@ -2,7 +2,26 @@ package com.practice;
 
 public class ColumnNameFromGivenColumnNumber {
 	public static void main(String args[]) {
-		
+		long n = 28;
+		String colName = getColName(n);
+		System.out.println("Column name for : " + n + " is :" + colName);
 	}
 
+	private static String getColName(long num) {
+		StringBuilder colName = new StringBuilder();
+
+		while (num > 0) {
+
+			int rem = (int) num % 26;
+
+			if (rem == 0) {
+				colName.append('Z');
+				num = (num / 26) - 1;
+			} else {
+				colName.append((char) ((rem - 1) + 'A'));
+				num = (num / 26);
+			}
+		}
+		return colName.reverse().toString();
+	}
 }
