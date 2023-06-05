3f92304adc49e3e1eaa74e918340302b94f62750
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-05 14:37:50 +0530

    Add files via upload

--- PangramChecking/src/com/practice/PangramChecking.java
+++ PangramChecking/src/com/practice/PangramChecking.java
@@ -1,8 +1,6 @@
 package com.practice;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class PangramChecking {
@@ -15,19 +13,15 @@ public class PangramChecking {
 
 	private static boolean checkIsPangram(String input) {
 		Map<String, Integer> charMap = new HashMap<String, Integer>();
-		
-		System.out.println((int)('A'));
-		System.out.println((int)('Z'));
-		System.out.println((int)('a'));
-		System.out.println((int)('z'));
-		
+
 		for (int i = 0; i < input.length(); i++) {
-			if(input.charAt(i)>64 && input.charAt(i)<91 || input.charAt(i)>96 && input.charAt(i)<123) {
-				charMap.put(String.valueOf(input.charAt(i)).toLowerCase(), charMap.getOrDefault(input.charAt(i), 0)+1);
+			if (input.charAt(i) > 64 && input.charAt(i) < 91 || input.charAt(i) > 96 && input.charAt(i) < 123) {
+				charMap.put(String.valueOf(input.charAt(i)).toLowerCase(),
+						charMap.getOrDefault(input.charAt(i), 0) + 1);
 			}
 		}
-		
-		if(charMap.size()==26) {
+
+		if (charMap.size() == 26) {
 			return true;
 		}
 		return false;
