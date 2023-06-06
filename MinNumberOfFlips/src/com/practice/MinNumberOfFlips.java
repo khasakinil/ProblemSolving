7d97d586b0ecbe7b5d5ffa5c5bd63e2b6e5039fe
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-06 09:39:16 +0530

    Min Number of Flips
    Given a binary string, that is it contains only 0s and 1s. We need to make this string a sequence of alternate characters by flipping some of the bits, our goal is to minimize the number of bits to be flipped.
    
    Example 1:
    
    Input:
    S = "001"
    Output: 1
    Explanation: 
    We can flip the 0th bit to 1 to have
    101.
    
    
    Example 2:
    
    Input:
    S = "0001010111" 
    Output: 2
    Explanation: We can flip the 1st and 8th bit 
    bit to have "0101010101"
    101.

--- MinNumberOfFlips/src/com/practice/MinNumberOfFlips.java
+++ MinNumberOfFlips/src/com/practice/MinNumberOfFlips.java
@@ -3,7 +3,7 @@ package com.practice;
 public class MinNumberOfFlips {
 	public static void main(String args[]) {
 
-		String input = "001";
+		String input = "0001010111";
 		int minNumberOfFlips = getMinNumberOfFlips(input);
 		System.out.println("minNumberOfFlips : " + minNumberOfFlips);
 	}
@@ -11,11 +11,37 @@ public class MinNumberOfFlips {
 	private static int getMinNumberOfFlips(String input) {
 		int firstFlip = 0;
 		int secondFlip = 0;
-		
-		for(int i=0; i<input.length();i++) {
-			
+
+		char nextFlipFirst = '0';
+		char nextFlipSecond = '1';
+
+		for (int i = 0; i < input.length(); i++) {
+
+			if (input.charAt(i) != nextFlipFirst) {
+				firstFlip++;
+			}
+
+			if (input.charAt(i) != nextFlipSecond) {
+				secondFlip++;
+			}
+
+			if (nextFlipFirst == '0') {
+				nextFlipFirst = '1';
+			} else {
+				nextFlipFirst = '0';
+			}
+
+			if (nextFlipSecond == '0') {
+				nextFlipSecond = '1';
+			} else {
+				nextFlipSecond = '0';
+			}
 		}
 
-		return 0;
+		if (firstFlip < secondFlip) {
+			return firstFlip;
+		}
+
+		return secondFlip;
 	}
 }
