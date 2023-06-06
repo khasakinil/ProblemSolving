9c30dc16806cef6bd67dcf237a99c66c6d80106d
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-06 09:18:34 +0530

    Add files via upload

---
+++ MinNumberOfFlips/src/com/practice/MinNumberOfFlips.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+public class MinNumberOfFlips {
+	public static void main(String args[]) {
+
+		String input = "001";
+		int minNumberOfFlips = getMinNumberOfFlips(input);
+		System.out.println("minNumberOfFlips : " + minNumberOfFlips);
+	}
+
+	private static int getMinNumberOfFlips(String input) {
+		int firstFlip = 0;
+		int secondFlip = 0;
+		
+		for(int i=0; i<input.length();i++) {
+			
+		}
+
+		return 0;
+	}
+}
