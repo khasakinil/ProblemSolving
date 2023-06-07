accb56dcd897597fa326c0ba003b74cc2ae82807
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-07 13:40:14 +0530

    Add Minimum Characters
    Given a string str, find the minimum characters to be added at front of the string to make it a palindrome.
    
    
    Example 1:
    
    Input:
    str = ABCD
    Output: 3
    Explanation: The resultant string 
    after adding 3 characters is DCBABCD.
    The minimum possible answer is 3.
    Example 2:
    
    Input:
    str = ABA
    Output: 0
    Explanation: The given string
    is already a palindrome.

--- AddMinimumCharactersToMakePalindrome/src/com/practice/AddMinimumCharactersToMakePalindrome.java
+++ AddMinimumCharactersToMakePalindrome/src/com/practice/AddMinimumCharactersToMakePalindrome.java
@@ -2,47 +2,23 @@ package com.practice;
 
 public class AddMinimumCharactersToMakePalindrome {
 	public static void main(String args[]) {
-		String input = "ABA";
+		String input = "A";
 		int minCharToMakePalindrome = getMinimumCharactersToMakePalindrome(input);
 		System.out.println("minCharToMakePalindrome : " + minCharToMakePalindrome);
 	}
 
 	private static int getMinimumCharactersToMakePalindrome(String input) {
-
-		int minChars = 0;
-		int palindromeTillLength = 0;
-//		if (isPalindrome(input)) {
-//			return 0;
-//		}
-
-//		for (int i = 1; i < input.length(); i++) {
-//			if (isPalindrome(input.substring(0, i))) {
-//				palindromeTillLength = i;
-//			}
-//		}
-		
-		for (int i = input.length(); i >= 0; i--) {
-			if (isPalindrome(input.substring(0, i))) {
-				palindromeTillLength = i;
-				break;
-			}
-		}
-		
-		minChars = input.length() - palindromeTillLength;
-		return minChars;
-	}
-
-	private static boolean isPalindrome(String input) {
-		boolean isPandrome = true;
-		int i = 0, j = input.length() - 1;
+		int minChar = 0, i = 0, j = input.length() - 1;
 		while (i < j) {
-			if (input.charAt(i) != input.charAt(j)) {
-				isPandrome = false;
-				break;
+			if (input.charAt(i) == input.charAt(j)) {
+				i++;
+				j--;
+			} else {
+				minChar++;
+				i = 0;
+				j = input.length() - minChar - 1;
 			}
-			i++;
-			j--;
 		}
-		return isPandrome;
+		return minChar;
 	}
 }
