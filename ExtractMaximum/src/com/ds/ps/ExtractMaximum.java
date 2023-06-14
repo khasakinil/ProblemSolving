e2c79378d7f3c23f822967263078474c2defb340
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-14 11:04:13 +0530

    Add files via upload

--- ExtractMaximum/src/com/ds/ps/ExtractMaximum.java
+++ ExtractMaximum/src/com/ds/ps/ExtractMaximum.java
@@ -11,17 +11,26 @@ public class ExtractMaximum {
 	private static int getMaxNumber(String exp) {
 		int max = 0;
 		boolean status = false;
+		int lastIndex = 0;
 		for (int i = 0; i < exp.length(); i++) {
-			if (exp.charAt(i) >= (int) ('0') && exp.charAt(i) <= (int) ('9')) {
+			if (exp.charAt(i) >= 48 && exp.charAt(i) <= 57) {
 				status = true;
+				lastIndex = 0;
 				for (int j = i + 1; j < exp.length(); j++) {
-					if (!(exp.charAt(j) >= (int) ('0') && exp.charAt(j) <= (int) ('9'))) {
+					if (!(exp.charAt(j) >= 48 && exp.charAt(j) <= 57)) {
 						int current = Integer.parseInt(exp.substring(i, j));
 						if (current > max) {
 							max = current;
 						}
 						i = j - 1;
+						lastIndex = j;
 						break;
+					}
+				}
+				if (lastIndex == 0) {
+					int current = Integer.parseInt(exp.substring(i, exp.length()));
+					if (current > max) {
+						max = current;
 					}
 				}
 			}
