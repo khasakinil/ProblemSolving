3a7384814199ced90fb9f7d707feda9f6ae5799c
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-19 09:26:21 +0530

    Add files via upload

--- MinimumIndexedCharacter/src/com/ds/ps/MinimumIndexedCharacter.java
+++ MinimumIndexedCharacter/src/com/ds/ps/MinimumIndexedCharacter.java
@@ -1,19 +1,20 @@
 package com.ds.ps;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MinimumIndexedCharacter {
 	public static void main(String[] args) {
-		String input1 = "";
-		String input2 = "";
+		String input1 = "adcffaet";
+		String input2 = "onkl";
 		int minimumIndexedCharacter = getMinimumIndexedCharacter(input1, input2);
 		System.out.println("MinimumIndexedCharacter : " + minimumIndexedCharacter);
 	}
 
 	private static int getMinimumIndexedCharacter(String input1, String input2) {
 
-		HashMap<Character, Integer> input1HashMap = new HashMap<>();
-		HashMap<Character, Integer> input2HashMap = new HashMap<>();
+		Map<Character, Integer> input1HashMap = new LinkedHashMap<>();
+		Map<Character, Integer> input2HashMap = new LinkedHashMap<>();
 
 		for (int i = 0; i < input1.length(); i++) {
 			input1HashMap.put(input1.charAt(i), input1HashMap.getOrDefault(input1.charAt(i), 0) + 1);
@@ -23,6 +24,31 @@ public class MinimumIndexedCharacter {
 			input2HashMap.put(input2.charAt(i), input2HashMap.getOrDefault(input2.charAt(i), 0) + 1);
 		}
 
-		return 0;
+		int count1 = 0;
+		int count2 = 0;
+		boolean input1Status = false;
+		boolean input2Status = false;
+
+		for (Map.Entry<Character, Integer> map : input1HashMap.entrySet()) {
+			count1++;
+			if (input2HashMap.containsKey(map.getKey())) {
+				input1Status = true;
+				break;
+			}
+		}
+
+		for (Map.Entry<Character, Integer> map : input2HashMap.entrySet()) {
+			count2++;
+			if (input1HashMap.containsKey(map.getKey())) {
+				input2Status = true;
+				break;
+			}
+		}
+
+		if (input1Status && input2Status && count1 != 0 && count2 != 0) {
+			return count1 < count2 ? count1 : count2;
+		}
+
+		return -1;
 	}
 }
