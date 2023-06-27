1b2a7efeb5c75318a316d5915515f0854dd889d3
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-27 22:10:31 +0530

    Add files via upload

--- MinimizeTheSumOfProduct/src/com/ds/ps/MinimizeTheSumOfProduct.java
+++ MinimizeTheSumOfProduct/src/com/ds/ps/MinimizeTheSumOfProduct.java
@@ -1,5 +1,14 @@
 package com.ds.ps;
 
 public class MinimizeTheSumOfProduct {
-	
+	public static void main(String args[]) {
+		int arr1[] = { 3, 1, 1 };
+		int arr2[] = { 6, 5, 4 };
+		long minimumProduct = minimizeTheSumOfProduct(arr1, arr2);
+		System.out.println("MinimumProduct : " + minimumProduct);
+	}
+
+	private static long minimizeTheSumOfProduct(int[] arr1, int[] arr2) {
+		return 0;
+	}
 }
