06a5d86e1997da102992a7574f639aa0d77ede81
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-27 22:21:59 +0530

    Add files via upload

--- MinimizeTheSumOfProduct/src/com/ds/ps/MinimizeTheSumOfProduct.java
+++ MinimizeTheSumOfProduct/src/com/ds/ps/MinimizeTheSumOfProduct.java
@@ -2,13 +2,40 @@ package com.ds.ps;
 
 public class MinimizeTheSumOfProduct {
 	public static void main(String args[]) {
-		int arr1[] = { 3, 1, 1 };
-		int arr2[] = { 6, 5, 4 };
+		long arr1[] = { 3, 1, 1 };
+		long arr2[] = { 6, 5, 4 };
 		long minimumProduct = minimizeTheSumOfProduct(arr1, arr2);
 		System.out.println("MinimumProduct : " + minimumProduct);
 	}
 
-	private static long minimizeTheSumOfProduct(int[] arr1, int[] arr2) {
-		return 0;
+	private static long minimizeTheSumOfProduct(long[] arr1, long[] arr2) {
+
+		long minimizedProduct = 0;
+
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = i + 1; j < arr1.length; j++) {
+				if (arr1[i] > arr1[j]) {
+					long temp = arr1[i];
+					arr1[i] = arr1[j];
+					arr1[j] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = i + 1; j < arr2.length; j++) {
+				if (arr2[i] < arr2[j]) {
+					long temp = arr2[i];
+					arr2[i] = arr2[j];
+					arr2[j] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < arr1.length; i++) {
+			minimizedProduct += (arr1[i] * arr2[i]);
+		}
+
+		return minimizedProduct;
 	}
 }
