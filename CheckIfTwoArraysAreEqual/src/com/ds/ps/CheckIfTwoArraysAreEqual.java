3c7b3cb0aceda85536a0a6b9d8b94e6c16dac452
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-28 22:54:54 +0530

    Add files via upload

---
+++ CheckIfTwoArraysAreEqual/src/com/ds/ps/CheckIfTwoArraysAreEqual.java
@@ -0,0 +1,40 @@
+package com.ds.ps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckIfTwoArraysAreEqual {
+	public static void main(String args[]) {
+		long arr1[] = { 1, 2, 5, 4, 0 };
+		long arr2[] = { 2, 4, 5, 0, 1 };
+
+		boolean isTwoArraysEqual = getTwoArraysEqualityStatus(arr1, arr2);
+		System.out.println("IsTwoArraysEqual : " + isTwoArraysEqual);
+	}
+
+	private static boolean getTwoArraysEqualityStatus(long[] arr1, long[] arr2) {
+
+		Map<Integer, Integer> arr1Map = new HashMap<>();
+		Map<Integer, Integer> arr2Map = new HashMap<>();
+
+		for (int i = 0; i < arr1.length; i++) {
+			arr1Map.put((int) arr1[i], arr1Map.getOrDefault(arr1[i], 0) + 1);
+		}
+
+		for (int i = 0; i < arr2.length; i++) {
+			arr2Map.put((int) arr2[i], arr2Map.getOrDefault(arr2[i], 0) + 1);
+		}
+
+		for (Map.Entry<Integer, Integer> map : arr1Map.entrySet()) {
+			if (arr2Map.get(map.getKey()) == null || arr2Map.get(map.getKey()) != map.getValue()) {
+				return false;
+			}
+		}
+
+		if (arr1Map.size() != arr2Map.size()) {
+			return false;
+		}
+
+		return true;
+	}
+}
