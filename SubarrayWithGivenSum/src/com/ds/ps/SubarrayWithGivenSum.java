5aaea34d4690dc485abe033abe6c19fad0ba7205
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-06-30 23:27:45 +0530

    Add files via upload

---
+++ SubarrayWithGivenSum/src/com/ds/ps/SubarrayWithGivenSum.java
@@ -0,0 +1,35 @@
+package com.ds.ps;
+
+import java.util.ArrayList;
+
+public class SubarrayWithGivenSum {
+	public static void main(String args[]) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int s = 15;
+		ArrayList<Integer> subArrayList = getSubarrayWithGivenSum(arr, s);
+		System.out.println("SubArrayList : " + subArrayList);
+	}
+
+	private static ArrayList<Integer> getSubarrayWithGivenSum(int[] arr, int s) {
+
+		ArrayList<Integer> subArrayList = new ArrayList<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			int arrSum = 0;
+			for (int j = i; j < arr.length; j++) {
+				arrSum += arr[j];
+				if (arrSum == s) {
+					subArrayList.add(i + 1);
+					subArrayList.add(j + 1);
+					return subArrayList;
+				}
+				if (arrSum > s) {
+					break;
+				}
+			}
+		}
+
+		subArrayList.add(-1);
+		return subArrayList;
+	}
+}
