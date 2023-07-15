f69a1e9a74440b84121061607dc8c1253935cf97
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-15 22:33:57 +0530

    Add files via upload

---
+++ FrequenciesOfLimitedRangeArrayElements/src/com/ds/ps/FrequenciesOfLimitedRangeArrayElements.java
@@ -0,0 +1,18 @@
+package com.ds.ps;
+
+public class FrequenciesOfLimitedRangeArrayElements {
+	public static void main(String args[]) {
+		int arr[] = { 2, 3, 2, 3, 5 };
+		int[] frequencyRange = getFrequenciesOfLimitedRangeArrayElements(arr);
+		System.out.println("FrequencyRange : " + frequencyRange);
+	}
+
+	private static int[] getFrequenciesOfLimitedRangeArrayElements(int[] arr) {
+		int[] response = new int[arr.length];
+		
+		for(int i=0; i<arr.length; i++) {
+		}
+		
+		return null;
+	}
+}
