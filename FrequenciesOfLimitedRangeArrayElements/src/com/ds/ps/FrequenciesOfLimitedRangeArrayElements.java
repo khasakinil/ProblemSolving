2f93f788ef12e1a67739f913de8b52dd1e25b45f
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-15 22:42:32 +0530

    Add files via upload

--- FrequenciesOfLimitedRangeArrayElements/src/com/ds/ps/FrequenciesOfLimitedRangeArrayElements.java
+++ FrequenciesOfLimitedRangeArrayElements/src/com/ds/ps/FrequenciesOfLimitedRangeArrayElements.java
@@ -4,15 +4,19 @@ public class FrequenciesOfLimitedRangeArrayElements {
 	public static void main(String args[]) {
 		int arr[] = { 2, 3, 2, 3, 5 };
 		int[] frequencyRange = getFrequenciesOfLimitedRangeArrayElements(arr);
-		System.out.println("FrequencyRange : " + frequencyRange);
+
+		for (int i = 0; i < frequencyRange.length; i++) {
+			System.out.println("FrequencyRange : " + frequencyRange[i]);
+		}
 	}
 
 	private static int[] getFrequenciesOfLimitedRangeArrayElements(int[] arr) {
 		int[] response = new int[arr.length];
-		
-		for(int i=0; i<arr.length; i++) {
+
+		for (int i = 0; i < arr.length; i++) {
+			response[arr[i] - 1] = response[arr[i] - 1] + 1;
 		}
-		
-		return null;
+
+		return response;
 	}
 }
--- FrequenciesOfLimitedRangeArrayElements/src/module-info.java
+++ FrequenciesOfLimitedRangeArrayElements/src/module-info.java
@@ -4,20 +4,23 @@
 /**
  * @author nilakanth
  *
- *         Given two unsorted arrays A of size N and B of size M of distinct
- *         elements, the task is to find all pairs from both arrays whose sum is
- *         equal to X.
+ *         Given an array A[] of N positive integers which can contain integers
+ *         from 1 to P where elements can be repeated or can be absent from the
+ *         array. Your task is to count the frequency of all elements from 1 to
+ *         N. Note: The elements greater than N in the array can be ignored for
+ *         counting and do modify the array in-place.
  * 
- *         Note: All pairs should be printed in increasing order of u. For eg.
- *         for two pairs (u1,v1) and (u2,v2), if u1 < u2 then (u1,v1) should be
- *         printed first else second.
  * 
  *         Example 1:
  * 
- *         Input: A[] = {1, 2, 4, 5, 7} B[] = {5, 6, 3, 4, 8} X = 9 Output: 1 8
- *         4 5 5 4 Explanation: (1, 8), (4, 5), (5, 4) are the pairs which sum
- *         to 9. Example 2: Input: A[] = {-1, -2, 4, -6, 5, 7} B[] = {6, 3, 4,
- *         0} X = 8 Output: 4 4 5 3
+ *         Input: N = 5 arr[] = {2, 3, 2, 3, 5} P = 5 Output: 0 2 2 0 1
+ *         Explanation: Counting frequencies of each array element We have: 1
+ *         occurring 0 times. 2 occurring 2 times. 3 occurring 2 times. 4
+ *         occurring 0 times. 5 occurring 1 time. Example 2:
+ * 
+ *         Input: N = 4 arr[] = {3,3,3,3} P = 3 Output: 0 0 4 0 Explanation:
+ *         Counting frequencies of each array element We have: 1 occurring 0
+ *         times. 2 occurring 0 times. 3 occurring 4 times. 4 occurring 0 times.
  *
  */
 module KeyPair {
