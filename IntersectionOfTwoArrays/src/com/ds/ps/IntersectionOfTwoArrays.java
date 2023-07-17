4520d28cfafbba5b9e4a75cf688ea02badb89dd8
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-17 22:57:12 +0530

    Add files via upload

--- IntersectionOfTwoArrays/src/com/ds/ps/IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays/src/com/ds/ps/IntersectionOfTwoArrays.java
@@ -1,16 +1,37 @@
 package com.ds.ps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IntersectionOfTwoArrays {
 	public static void main(String args[]) {
-		long arr1[] = { 89, 24, 75, 11, 23 };
-		long arr2[] = { 89, 2, 4 };
+		int arr1[] = { 1, 2, 3, 4, 5, 6 };
+		int arr2[] = { 7 };
 
 		int intersectionOfTwoArrays = getCountIntersectionOfTwoArrays(arr1, arr2);
 		System.out.println("IntersectionOfTwoArrays : " + intersectionOfTwoArrays);
 	}
 
-	private static int getCountIntersectionOfTwoArrays(long[] arr1, long[] arr2) {
-		// TODO Auto-generated method stub
-		return 0;
+	private static int getCountIntersectionOfTwoArrays(int[] arr1, int[] arr2) {
+		Map<Integer, Integer> arr1Map = new HashMap<>();
+		Map<Integer, Integer> arr2Map = new HashMap<>();
+
+		int intersection = 0;
+
+		for (int i = 0; i < arr1.length; i++) {
+			arr1Map.put(arr1[i], arr1Map.getOrDefault(arr1[i], 0) + 1);
+		}
+
+		for (int i = 0; i < arr2.length; i++) {
+			arr2Map.put(arr2[i], arr2Map.getOrDefault(arr2[i], 0) + 1);
+		}
+
+		for (Map.Entry<Integer, Integer> map : arr1Map.entrySet()) {
+			if (arr2Map.containsKey(map.getKey())) {
+				intersection += Math.min((int) (map.getValue()), arr2Map.get(map.getKey()));
+			}
+		}
+
+		return intersection;
 	}
 }
