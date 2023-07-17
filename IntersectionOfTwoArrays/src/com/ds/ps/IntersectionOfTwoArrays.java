d09029acee692dfc8b22fca62c0ae4f7cae4b34d
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-17 22:41:16 +0530

    Add files via upload

---
+++ IntersectionOfTwoArrays/src/com/ds/ps/IntersectionOfTwoArrays.java
@@ -0,0 +1,16 @@
+package com.ds.ps;
+
+public class IntersectionOfTwoArrays {
+	public static void main(String args[]) {
+		long arr1[] = { 89, 24, 75, 11, 23 };
+		long arr2[] = { 89, 2, 4 };
+
+		int intersectionOfTwoArrays = getCountIntersectionOfTwoArrays(arr1, arr2);
+		System.out.println("IntersectionOfTwoArrays : " + intersectionOfTwoArrays);
+	}
+
+	private static int getCountIntersectionOfTwoArrays(long[] arr1, long[] arr2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
