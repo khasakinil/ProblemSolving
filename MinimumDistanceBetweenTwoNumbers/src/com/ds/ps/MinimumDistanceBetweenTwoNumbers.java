812045d04b0620cde2ef4275c0c5952fc3c8d829
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-21 22:33:42 +0530

    Add files via upload

--- MinimumDistanceBetweenTwoNumbers/src/com/ds/ps/MinimumDistanceBetweenTwoNumbers.java
+++ MinimumDistanceBetweenTwoNumbers/src/com/ds/ps/MinimumDistanceBetweenTwoNumbers.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 public class MinimumDistanceBetweenTwoNumbers {
 	public static void main(String args[]) {
-		int arr[] = { 1, 2, 3, 2 };
-		int num1 = 42;
-		int num2 = 12;
+		int arr[] = { 96, 82, 48, 76, 34, 19, 7, 80, 36, 57, 77, 34, 19, 35, 5, 57, 16, 66, 42, 62, 89, 19, 60, 19, 25,
+				16, 20, 51, 77, 75, 12, 73, 8, 11, 100, 93, 81, 58, 72, 17, 14, 48, 2, 33, 82, 6, 41, 49, 72, 34, 10,
+				12, 53, 21, 30, 77, 36, 49, 79, 13, 75, 42 };
+		int num1 = 36;
+		int num2 = 33;
 		int minimumDistanceBetweenTwoNumbers = getMinimumDistanceBetweenTwoNumbers(arr, num1, num2);
 		System.out.println("MinimumDistanceBetweenTwoNumbers : " + minimumDistanceBetweenTwoNumbers);
 	}
@@ -16,7 +18,7 @@ public class MinimumDistanceBetweenTwoNumbers {
 		Map<Integer, Integer> numMap = new HashMap<>();
 		numMap.put(num1, Integer.MIN_VALUE);
 		numMap.put(num2, Integer.MIN_VALUE);
-
+		int minDistance = Integer.MAX_VALUE;
 		for (int i = 0; i < arr.length; i++) {
 			if (num1 == arr[i]) {
 				numMap.put(num1, i);
@@ -27,10 +29,17 @@ public class MinimumDistanceBetweenTwoNumbers {
 			}
 
 			if (numMap.get(num1) != Integer.MIN_VALUE && numMap.get(num2) != Integer.MIN_VALUE) {
-				return Math.abs(numMap.get(num1) - numMap.get(num2));
-			}
 
+				minDistance = minDistance > Math.abs(numMap.get(num1) - numMap.get(num2))
+						? Math.abs(numMap.get(num1) - numMap.get(num2))
+						: minDistance;
+			}
 		}
-		return -1;
+
+		if (minDistance == Integer.MAX_VALUE) {
+			return -1;
+		}
+
+		return minDistance;
 	}
 }
