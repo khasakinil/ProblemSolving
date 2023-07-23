2f68d46c349b37f3771394cd0a35406caf1fd8d3
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-23 22:44:53 +0530

    Add files via upload

--- FirstNegativeIntegerInEveryWindowSize/src/com/ds/ps/FirstNegativeIntegerInEveryWindowSize.java
+++ FirstNegativeIntegerInEveryWindowSize/src/com/ds/ps/FirstNegativeIntegerInEveryWindowSize.java
@@ -1,17 +1,32 @@
 package com.ds.ps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirstNegativeIntegerInEveryWindowSize {
 	public static void main(String args[]) {
 		long arr[] = { -8, 2, 3, -6, 10 };
 		int k = 2;
-		long firstNegativeInWindow[] = getFirstNegativeIntegerInEveryWindowSize(arr, k);
+		List<Long> firstNegativeInWindow = getFirstNegativeIntegerInEveryWindowSize(arr, k);
 		System.out.println("FirstNegativeInWindow : " + firstNegativeInWindow);
 	}
 
-	private static long[] getFirstNegativeIntegerInEveryWindowSize(long[] arr, int k) {
-		
-		
-		
-		return null;
+	private static List<Long> getFirstNegativeIntegerInEveryWindowSize(long[] arr, int k) {
+		List<Long> firstNegativeInWindow = new ArrayList<>();
+
+		for (long i = 0; i < arr.length; i++) {
+			if (k + i >= arr.length) {
+				break;
+			}
+			long small = 0;
+			for (long j = i; j <= i + k; j++) {
+				if (arr[(int) j] < 0) {
+					small = arr[(int) j];
+				}
+			}
+			firstNegativeInWindow.add(small);
+		}
+
+		return firstNegativeInWindow;
 	}
 }
