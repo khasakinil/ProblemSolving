360713aca05e2e75642ac0ad509907e527d59a6a
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-23 22:04:59 +0530

    Add files via upload

---
+++ FirstNegativeIntegerInEveryWindowSize/src/com/ds/ps/FirstNegativeIntegerInEveryWindowSize.java
@@ -0,0 +1,17 @@
+package com.ds.ps;
+
+public class FirstNegativeIntegerInEveryWindowSize {
+	public static void main(String args[]) {
+		long arr[] = { -8, 2, 3, -6, 10 };
+		int k = 2;
+		long firstNegativeInWindow[] = getFirstNegativeIntegerInEveryWindowSize(arr, k);
+		System.out.println("FirstNegativeInWindow : " + firstNegativeInWindow);
+	}
+
+	private static long[] getFirstNegativeIntegerInEveryWindowSize(long[] arr, int k) {
+		
+		
+		
+		return null;
+	}
+}
---
+++ FirstNegativeIntegerInEveryWindowSize/src/module-info.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+/**
+ * @author nilakanth
+ *
+ *         Given an array A[] of size N and a positive integer K, find the first
+ *         negative integer for each and every window(contiguous subarray) of
+ *         size K.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input : N = 5 A[] = {-8, 2, 3, -6, 10} K = 2 Output : -8 0 -6 -6
+ *         Explanation : First negative integer for each window of size k {-8,
+ *         2} = -8 {2, 3} = 0 (does not contain a negative integer) {3, -6} = -6
+ *         {-6, 10} = -6
+ * 
+ *         Example 2: Input : N = 8 A[] = {12, -1, -7, 8, -15, 30, 16, 28} K = 3
+ *         Output : -1 -1 -7 -15 -15 0
+ * 
+ * 
+ */
+module CheckIfTwoArraysAreEqual {
+}
