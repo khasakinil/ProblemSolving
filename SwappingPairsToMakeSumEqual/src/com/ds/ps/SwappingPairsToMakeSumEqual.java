65a765801684f33ebba30bb5c3e8a9c89896ea5e
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-26 22:19:44 +0530

    Add files via upload

---
+++ SwappingPairsToMakeSumEqual/src/com/ds/ps/SwappingPairsToMakeSumEqual.java
@@ -0,0 +1,34 @@
+package com.ds.ps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SwappingPairsToMakeSumEqual {
+	public static void main(String args[]) {
+		long arr1[] = { 4, 1, 2, 1, 1, 2 };
+		long arr2[] = { 3, 6, 3, 3 };
+		long isSwapMakeSumEqual = isSwappingPairsToMakeSumEqual(arr1, arr2);
+		System.out.println("isSwapMakeSumEqual : " + isSwapMakeSumEqual);
+	}
+
+	private static long isSwappingPairsToMakeSumEqual(long[] arr1, long[] arr2) {
+
+		long arr1Sum = 0;
+		long arr2Sum = 0;
+
+		Map<Integer, Integer> map1Arr = new HashMap<>();
+		Map<Integer, Integer> map2Arr = new HashMap<>();
+
+		for (int i = 0; i < arr1.length; i++) {
+			arr1Sum += arr1[i];
+			map1Arr.put((int) arr1[i], map1Arr.getOrDefault(arr1[i], 0) + 1);
+		}
+
+		for (int i = 0; i < arr2.length; i++) {
+			arr2Sum += arr2[i];
+			map2Arr.put((int) arr2[i], map2Arr.getOrDefault(arr2[i], 0) + 1);
+		}
+
+		return 0;
+	}
+}
---
+++ SwappingPairsToMakeSumEqual/src/module-info.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+/**
+ * @author nilakanth
+ *
+ *         Given two arrays of integers A[] and B[] of size N and M, the task is
+ *         to check if a pair of values (one value from each array) exists such
+ *         that swapping the elements of the pair will make the sum of two
+ *         arrays equal.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input: N = 6, M = 4 A[] = {4, 1, 2, 1, 1, 2} B[] = (3, 6, 3, 3)
+ *         Output: 1 Explanation: Sum of elements in A[] = 11 Sum of elements in
+ *         B[] = 15, To get same sum from both arrays, we can swap following
+ *         values: 1 from A[] and 3 from B[] Example 2:
+ * 
+ *         Input: N = 4, M = 4 A[] = {5, 7, 4, 6} B[] = {1, 2, 3, 8} Output: 1
+ *         Explanation: We can swap 6 from array A[] and 2 from array B[]
+ * 
+ */
+module CheckIfTwoArraysAreEqual {
+}
