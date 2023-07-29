7badd76de7b8a632e636c5c2bd2d0d9b325bd9f0
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-07-29 17:56:36 +0000

    Add files via upload

---
+++ CountOddEven/src/module-info.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+/**
+ * @author nilakanth
+ *
+ *         Given an array A[] of N elements. The task is to count number of even
+ *         and odd elements in the array.
+ * 
+ *         Example:
+ * 
+ *         Input: N = 5 A[] = 1 2 3 4 5 Output: 3 2 Explanation: There are 3 odd
+ *         elements (1, 3, 5) and 2 even elements (2 and 4).
+ *
+ * 
+ */
+module LeadersInAnArray {
+}
