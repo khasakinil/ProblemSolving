a577383fb9686f9ea33aa4b26bcce274183018e5
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-08-05 21:31:46 +0530

    Add files via upload

---
+++ CountPairsWithGivenSum/src/com/ds/ps/CountPairsWithGivenSum.java
@@ -0,0 +1,15 @@
+package com.ds.ps;
+
+public class CountPairsWithGivenSum {
+	public static void main(String args[]) {
+		int arr[] = { 1, 1, 1, 1 };
+		int sum = 2;
+		int pairCount = getPairsCount(arr, sum);
+		System.out.println("PairCount : " + pairCount);
+	}
+
+	private static int getPairsCount(int[] arr, int sum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
