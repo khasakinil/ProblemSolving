4b08aadbb332f24e4fca4980691dc80fe537703e
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-08-12 23:03:27 +0530

    Add files via upload

---
+++ MultiplyTwoStrings/src/com/practice/MultiplyTwoStrings.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class MultiplyTwoStrings {
+	public static void main(String args[]) {
+		String str1 = "0033";
+		String str2 = "2";
+		String strMultiply = getStringsMultiply(str1, str2);
+		System.out.println("Multiple of two Strings : " + strMultiply);
+	}
+
+	private static String getStringsMultiply(String str1, String str2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
